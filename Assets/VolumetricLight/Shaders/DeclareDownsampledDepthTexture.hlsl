10cbae77e3bb39ea8728ddee601aa49e02c12fb8
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-12-30 14:06:16 +0900

    추가 | 에셋

---
+++ Assets/VolumetricLight/Shaders/DeclareDownsampledDepthTexture.hlsl
@@ -0,0 +1,15 @@
+#ifndef UNITY_DECLARE_DOWNSAMPLED_DEPTH_TEXTURE_INCLUDED
+#define UNITY_DECLARE_DOWNSAMPLED_DEPTH_TEXTURE_INCLUDED
+
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/GlobalSamplers.hlsl"
+
+TEXTURE2D_X_FLOAT(_HalfResCameraDepthTexture);
+float4 _HalfResCameraDepthTexture_TexelSize;
+
+// Samples the half resolution camera depth texture.
+float SampleDownsampledSceneDepth(float2 uv)
+{
+	return SAMPLE_TEXTURE2D_X(_HalfResCameraDepthTexture, sampler_PointClamp, uv).r;
+}
+
+#endif
