bd892b490f59695d716bc19e33eca55c20084b7f
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-12-31 13:42:23 +0900

    추가 | 컷씬

--- Assets/00.Main/00.Script/CutSceneCameraShake.cs
+++ Assets/00.Main/00.Script/CutSceneCameraShake.cs
@@ -3,36 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraShake : MonoBehaviour
+public class CutSceneCameraShake : MonoBehaviour
 {
 
-    public static CameraShake instance;
 
-    private CinemachineVirtualCamera cinemachineVirtualCamera;
+    public CinemachineVirtualCamera cinemachineVirtualCamera;
     private float shakeTimer;
     // Start is called before the first frame update
     void Awake()
     {
-        instance = this;
-        cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
     }
 
     public void ShakeCamera(float instensity, float time)
     {
-        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = 
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
             cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
 
         cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = instensity;
         shakeTimer = time;
     }
 
- 
+    public void VNShakeCamera()
+    {
+        ShakeCamera(3f, 0.2f);
+    }
+
     private void Update()
     {
         if (shakeTimer > 0)
         {
             shakeTimer -= Time.deltaTime;
-            if(shakeTimer <= 0f)
+            if (shakeTimer <= 0f)
             {
                 CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
           cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
